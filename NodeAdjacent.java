e8eb8c5dde53a09f5aaabe1a51c35e7ff0ee257b
abdulist <dexel7zip@gmail.com>
2022-06-10 00:50:34 +0700

    Jurnal13

---
+++ NodeAdjacent.java
@@ -0,0 +1,21 @@
+public class NodeAdjacent {
+
+ private int data;
+ private boolean visited=false;
+
+ NodeAdjacent(int data) {
+  this.data = data;
+  }
+public int getData() {
+  return data;
+  }
+
+ public boolean isVisited() {
+ return visited;
+ }
+
+ public void setVisited(boolean visited) {
+ this.visited = visited;
+ }
+ }
+
